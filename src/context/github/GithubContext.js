4e5a4d667d5109021c414eb4f2e1bc0acc757ffe
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-28 16:43:14 -0700

    User Repos and display

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -1,4 +1,5 @@
 import { createContext, useReducer } from "react";
+import { createRenderer } from "react-dom/test-utils";
 import githubReducer from "./GithubReducer";
 
 const GithubContext = createContext()
@@ -9,6 +10,7 @@ export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
         user: {},
+        repos: [],
         loading: false,
     }
 
@@ -47,8 +49,25 @@ export const GithubProvider = ({children}) => {
                 payload: data,
             })
         }
+    }
 
+    //Get User Repos
+    const getUserRepos = async (login) => {
+        setLoading()
 
+        const params = new URLSearchParams({
+            sort: 'created',
+            per_page: 10
+        })
+
+        const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`)
+
+        const data = await response.json()
+        
+        dispatch({
+            type: 'GET_REPOS',
+            payload: data,
+        })
     }
 
 //Clear search results
@@ -64,9 +83,11 @@ export const GithubProvider = ({children}) => {
         users: state.users,
         loading: state.loading,
         user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
         getUser,
+        getUserRepos,
     }}>
         {children}
     </GithubContext.Provider>
