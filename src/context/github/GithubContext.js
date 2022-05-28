44c1d6d4a3681cf38c4e64d1361f0f7f47d1faf3
Meg <92758663+mbarnstien@users.noreply.github.com>
2022-05-28 14:20:31 -0700

    Search and clear users

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -8,26 +8,43 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL
 export const GithubProvider = ({children}) => {
     const initialState = {
         users: [],
-        loading: true,
+        loading: false,
     }
 
     const [state, dispatch] = useReducer(githubReducer, initialState)
 
-    const fetchUsers = async () => {
-        const response = await fetch(`${GITHUB_URL}/users`)
+//Get search results
+    const searchUsers = async (text) => {
+        setLoading()
 
-        const data = await response.json()
+        const params = new URLSearchParams({
+            q: text
+        })
+
+        const response = await fetch(`${GITHUB_URL}/search/users?${params}`)
+
+        const {items} = await response.json()
         
         dispatch({
             type: 'GET_USERS',
-            payload: data,
+            payload: items,
         })
     }
+
+//Clear search results
+    const clearUsers = () =>
+        dispatch({
+            type: 'CLEAR_USERS'
+        })
+
+
+    const setLoading = () => dispatch({type: 'SET_LOADING'})
 
     return <GithubContext.Provider value={{
         users: state.users,
         loading: state.loading,
-        fetchUsers,
+        searchUsers,
+        clearUsers,
     }}>
         {children}
     </GithubContext.Provider>
